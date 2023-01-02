dd9b71c346b55421e4b2b0481b08c3ef13b7a5dc
Pellehakman <perhakman@outlook.com>
2023-01-02 12:08:22 +0100

    1

--- src/views/Home/Home.ts
+++ src/views/Home/Home.ts
@@ -4,5 +4,16 @@ export default defineComponent({
     name: "Home",
     setup(){
 
+
+        useEffect(() => {
+            const auth = localStorage.getItem('user')
+            if (auth) {
+              navigate('/admin')
+              setIsAdminView(true)
+            } else {
+              navigate('/login')
+            }
+          }, [])
+
     }
 })
--- src/views/Login/Login.ts
+++ src/views/Login/Login.ts
@@ -1,3 +1,4 @@
+
 import { defineComponent, ref } from "vue";
 import type { Credentials } from '../../models/data'
 
@@ -5,46 +6,50 @@ export default defineComponent({
     name: "Login",
     setup(){
         
-        
+      const username = ref('')      
+      const password = ref('')
+
          async function handleSubmit(){
-            // console.log(username.value, password.value)
+            console.log(username.value, password.value)
 
-            const username = ref('')      
-            const password = ref('')
+          
 
-            let user: Credentials = {
+            let user = {
                 username: username.value,
                 password: password.value,
-            };
-          
+            }
+          // console.log(user)
               //skickar user till /api/credentials
               const sendingCredentials = {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(user),
               };
+             
           
               const credentialsResult = await fetch(
                 "/api/credentials",
                 sendingCredentials
               );
+
+              console.log(credentialsResult)
               if (credentialsResult.status === 200) {
                 localStorage.setItem("user", JSON.stringify(credentialsResult));
-                
+                redirect("/home");
               } else {
-                //visar användaren om den skrivit in fel användarnamn/lösenord
+                alert('you have done bad')
                 
               }
 
-
-
+             
+             
 
            
     }
-        return {
-            username,
-            password,
-            handleSubmit
-        }
+    return {
+      username,
+      password,
+      handleSubmit
+  }
     }
-}
+})
